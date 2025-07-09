73fa5bcae9a44161a6af75b8161bc212f882c3da
naderhaji <nader9hajji@gmail.com>
2025-07-09 19:07:19 +0100

    Interface employee

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { Employee } from '../../model/Employee';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule,CommonModule, DatePipe ],
   templateUrl: './employee.component.html',
-  styleUrl: './employee.component.css'
+  styleUrl: './employee.component.css',
 })
 export class EmployeeComponent {
+  employees: Employee[] = [];
 
+  // Formulaire lié à l'objet
+  newEmployee: Employee = new Employee();
+
+  selectedIndex: number | null = null;
+
+  addRecord(): void {
+    if (this.selectedIndex !== null) {
+      // Modifier
+      this.employees[this.selectedIndex] = { ...this.newEmployee };
+      this.selectedIndex = null;
+    } else {
+      // Ajouter
+      this.employees.push({ ...this.newEmployee });
+    }
+
+    this.resetForm();
+  }
+
+  editRecord(index: number): void {
+    this.selectedIndex = index;
+    this.newEmployee = { ...this.employees[index] };
+  }
+
+  deleteRecord(index: number): void {
+    if (confirm('Are you sure you want to delete this employee?')) {
+      this.employees.splice(index, 1);
+      this.resetForm();
+    }
+  }
+
+  resetForm(): void {
+    this.newEmployee = new Employee();
+    this.selectedIndex = null;
+  }
 }
