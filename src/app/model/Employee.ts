507833d488ff60b1b60a40dcb6906ea27e6fd108
naderhaji <nader9hajji@gmail.com>
2025-07-10 18:18:19 +0100

    api getParentDeptList

--- src/app/model/Employee.ts
+++ src/app/model/Employee.ts
@@ -21,3 +21,21 @@ export class Employee {
     this.createdDate = new Date();
   }
 }
+
+export interface IParentDept {
+  departmentId: number;
+  departmentName: string;
+  departmentLogo: string;
+}
+
+export interface IChildDept {
+  childDeptId: number;
+  parentDeptId: number;
+  departmentName: string;
+}
+
+export interface IApiResponse {
+  message: string;
+  result: boolean;
+  data: any;
+}
